d904c56c477e7ebd75ed7812b9bde449a8cc8d8e
kessvan <kesavans255@gmail.com>
2022-10-18 20:21:15 +0530

    code Word

---
+++ Man.java
@@ -0,0 +1,15 @@
+class Man extends Human2
+{
+   void walk()
+{
+    System.out.println("Man walking slowly");
+
+}
+
+  
+   public static void main(String args[])
+   {  
+      Man obj = new Man();  
+      obj.walk();  
+   }
+}

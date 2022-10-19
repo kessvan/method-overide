383fa3d8d60443525c8b45ff669e75e3591ea949
kessvan <kesavans255@gmail.com>
2022-10-19 08:30:53 +0530

    'first

---
+++ Home.java
@@ -0,0 +1,15 @@
+class Home
+{
+public static void main(String []args)
+{
+int age=21;
+byte age1=21;
+short age2=21;
+long age3=21;
+double age4=21d;
+float age5=21f;
+char let='k';
+String name="kesavan";
+System.out.println("age ="+age+"\n"+"age1 ="+age1+"\n"+"age2 ="+age2+"\n"+"age3 ="+age3+"\n"+"age4 ="+age4+"\n"+"age5 ="+age5+"\n"+"let ="+let+ "\n"+"name ="+name);
+}
+}

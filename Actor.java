ca6b08ce444faf7db9899366a8110d700447e1ba
kessvan <kesavans255@gmail.com>
2022-11-20 21:00:03 +0530

    code

---
+++ Actor.java
@@ -0,0 +1,22 @@
+class Actor
+{
+boolean makeUpRequired=true;
+String address ="chennai";
+
+
+public void act()
+{
+System.out.println("acting");
+
+}
+public void dance()
+{
+
+System.out.println("dancing");
+}
+public void sing()
+{
+System.out.println("sing");
+
+}
+}
